10c754a83f15c2115a7ef5061abbb6e335b1afb4
Vladyslav Velytskyi <Vladyslav_Velytskyi@epam.com>
2021-01-25 16:29:34 +0200

    Add mock file for currency

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-jest.mock('@folio/stripes/smart-components'/*, () => ({
+jest.mock('@folio/stripes/smart-components', () => ({
   ...jest.requireActual('@folio/stripes/smart-components'),
-  AddressEdit: () => <div>AddressEdit</div>,
-  ControlledVocab: () => <div>ControlledVocab</div>,
-  Settings: props => <div>Settings</div>
-}), { virtual: true }*/);
+  //AddressEdit: () => <div>AddressEdit</div>,
+  //ControlledVocab: () => <div>ControlledVocab</div>,
+  //Settings: props => <div>Settings</div>
+}), { virtual: true });
