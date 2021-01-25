258e64834bb63eb0aebce3240213f66de51a1489
Vladyslav Velytskyi <Vladyslav_Velytskyi@epam.com>
2021-01-25 15:44:49 +0200

    Update mock file

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-jest.mock('@folio/stripes/smart-components', () => ({
+jest.mock('@folio/stripes/smart-components'/*, () => ({
   ...jest.requireActual('@folio/stripes/smart-components'),
-  //ControlledVocab: () => <div>ControlledVocab</div>,
-  //Settings: props => <div>Settings</div>
-}), { virtual: true });
+  AddressEdit: () => <div>AddressEdit</div>,
+  ControlledVocab: () => <div>ControlledVocab</div>,
+  Settings: props => <div>Settings</div>
+}), { virtual: true }*/);
